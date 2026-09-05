c56446e6b2067d0ee49201b004495bad6532c286
Reddawn <35641620+forumevi@users.noreply.github.com>
2026-09-05 23:02:55 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     config.resolve.fallback = { fs: false, net: false, tls: false };
